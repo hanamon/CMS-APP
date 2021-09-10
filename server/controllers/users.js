2ea996a3b6d000475e5b444c6ad3fc5fc55a496a
hanamon <devparkhana@gmail.com>
2021-09-10 17:48:08 +0900

    [UPDATE] users pagination

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,21 +6,19 @@ module.exports = {
   get: async (req, res) => {
     try {
       // 관리자 인증 검사
-      const userInfo = await adminToken(req, res);
+      //const userInfo = await adminToken(req, res);
       
       const queryKeys = Object.keys(req.query);
       const queryValues = Object.values(req.query);
-      const postAttribu = User.rawAttributes;
-      
-      // 쿼리가 없는 경우 모든 users 리턴
-      if( !queryKeys.length ) {
-        const users = await User.findAll({ attributes: ['user_login', 'user_name', 'user_email', 'user_nickname', 'user_role', 'user_status', 'createdAt'] });
-        return res.status(200).json({ data: { users }, message: 'ok' });
-      }
 
-      // 쿼리가 있는 경우 유효한 쿼리만 객체로 만들어서 배열에 담는다.
+      // 페이지네이션 처리
+      let { paged, post_number } = req.query;
+      isNaN(paged) || Number(paged) < 1 ? paged = 1 : paged = Number(paged);
+      isNaN(post_number) ? post_number = 5 : post_number < 1 ? post_number = 1 : post_number = Number(post_number);
+      
+      // DB에서 검색 가능한 쿼리만 객체로 만들어서 배열에 담는다.
       const queryArr = queryKeys.reduce((acc, cur, idx) => {
-        if( postAttribu[cur] ) {
+        if( User.rawAttributes[cur] ) {
           const obj = {};
           obj[cur] = queryValues[idx]
           return [...acc, obj];
@@ -28,9 +26,16 @@ module.exports = {
         return acc;
       }, []);
 
-      // 배열에 담긴 쿼리에 해당하는 포스트를 찾아서 리턴한다.
-      const users = await User.findAll({ where: { [Op.and]: queryArr }, attributes: ['user_login', 'user_name', 'user_email', 'user_nickname', 'user_role', 'user_status', 'createdAt'] });
-      return res.status(200).json({ data: { users }, message: 'ok' });
+      if( !queryArr.length ) {
+        // DB에서 검색 가능한 쿼리가 없는 경우 모든 유저를 리턴한다. (비밀번호 제거)
+        const users = await User.findAndCountAll({ attributes: ['user_login', 'user_name', 'user_email', 'user_nickname', 'user_role', 'user_status', 'createdAt'], offset: (paged-1)*post_number, limit: post_number });
+        return res.status(200).json({ data: { users }, message: 'ok' });
+      }
+      else {
+        // DB에서 검색 가능한 쿼리가 있는 경우 쿼리에 해당하는 포스트를 찾아서 리턴한다.
+        const users = await User.findAndCountAll({ attributes: ['user_login', 'user_name', 'user_email', 'user_nickname', 'user_role', 'user_status', 'createdAt'], where: { [Op.and]: queryArr }, offset: (paged-1)*post_number, limit: post_number });
+        return res.status(200).json({ data: { users }, message: 'ok' });
+      }
     }
     catch (err) {
       console.error(err);
