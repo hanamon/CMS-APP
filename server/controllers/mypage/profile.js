3c8ab001b7fbfdfe79d450532fefa497f67487a5
hanamon <devparkhana@gmail.com>
2021-10-03 23:27:31 +0900

    [ADD] mypage profile update

--- server/controllers/mypage/profile.js
+++ server/controllers/mypage/profile.js
@@ -1,3 +1,4 @@
+const { User } = require('module');
 const userAuthen = require('../authentication/userAuthen');
 
 module.exports = {
@@ -12,6 +13,26 @@ module.exports = {
     }
     catch (err) {
       console.error(err);
+    }
+  },
+  put: async (req, res) => {
+    try {
+      // 로그인 인증 검사
+      const userInfo = await userAuthen(req, res);
+
+      // 변경할 회원 정보가 없다면 다음을 리턴한다.
+      const { user_login, user_nickname } = req.body;
+      if( !user_login || !user_nickname ) return res.status(400).json({ message: 'Bad Request' });
+
+      // 변경이 가능하다면 변경한 후 변경 된 회원 정보를 반환한다.
+      const newUserInfo = await User.update({ user_nickname }, { where: user_login });
+      //if( result ) return res.status(409).json({ message: 'Conflict' });
+
+      delete newUserInfo.dataValues.user_pass;
+      res.status(201).json({ data: { userInfo: newUserInfo.dataValues }, message: 'ok' });
+    }
+    catch (err) {
+      console.log(err);
     }
   }
 }
